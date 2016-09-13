0a688533c6c97b905c64b283545564f559daa047
Hamid <7atashfeshan@gmail.com>
2016-09-13 15:59:14 +0430

    Migrated  /pipes cmds gofmt

--- pipes/cmds_messages.go
+++ pipes/cmds_messages.go
@@ -1,102 +1,101 @@
 package pipes
 
 import (
-    "ms/sun/base"
-    //"time"
-    "fmt"
-    "ms/sun/models"
-    "encoding/json"
-    "ms/sun/helper"
-//"ms/sun/docs/del/chat"
-    "ms/sun/sync"
-    "ms/sun/constants"
-    "ms/sun/commands"
+	"ms/sun/base"
+	//"time"
+	"encoding/json"
+	"fmt"
+	"ms/sun/helper"
+	"ms/sun/models"
+	//"ms/sun/docs/del/chat"
+	"ms/sun/commands"
+	"ms/sun/constants"
+	"ms/sun/sync"
 )
 
 func MsgAddNew(c *base.CmdAction) {
-    fmt.Println("called MsgAddNew :",*c)
+	fmt.Println("called MsgAddNew :", *c)
 
-    myMsg := models.MessagesTableFromClient{}
-    json.Unmarshal([]byte(c.Cmd.Data),&myMsg)
+	myMsg := models.MessagesTableFromClient{}
+	json.Unmarshal([]byte(c.Cmd.Data), &myMsg)
 
-    toUid := helper.StrToInt(myMsg.RoomKey[1:],-1)
-    if toUid < 1{
-        return
-    }
+	toUid := helper.StrToInt(myMsg.RoomKey[1:], -1)
+	if toUid < 1 {
+		return
+	}
 
-    //msg: send to peer
-    msg := models.CreateNewMsgRecivedForSendingToPeer(&myMsg,toUid,c.UserId) //ref
-    meta := models.CreateMsgRecivedToServerMetaResponse(&myMsg) //ref
+	//msg: send to peer
+	msg := models.CreateNewMsgRecivedForSendingToPeer(&myMsg, toUid, c.UserId) //ref
+	meta := models.CreateMsgRecivedToServerMetaResponse(&myMsg)                //ref
 
-    recivedCmd := commands.NewMsgsReceivedToServer(meta)
+	recivedCmd := commands.NewMsgsReceivedToServer(meta)
 
-    sync.AllPipesMap.SendAndStoreCmdToUser(c.UserId,recivedCmd)
+	sync.AllPipesMap.SendAndStoreCmdToUser(c.UserId, recivedCmd)
 
-    //send msg to peer
-    cmd := commands.NewMsgsAddNew(msg)
-    sync.AllPipesMap.SendAndStoreCmdToUser(toUid,cmd)
+	//send msg to peer
+	cmd := commands.NewMsgsAddNew(msg)
+	sync.AllPipesMap.SendAndStoreCmdToUser(toUid, cmd)
 }
 
-
 func MsgReceivedToPeer(c *base.CmdAction) {
-    fmt.Printf("\ncalled MsgReceivedToPeer %v :\n",c)
+	fmt.Printf("\ncalled MsgReceivedToPeer %v :\n", c)
 
-    metas := []models.MessageSyncMeta{}
+	metas := []models.MessageSyncMeta{}
 
-    json.Unmarshal([]byte(c.Cmd.Data),&metas)
+	json.Unmarshal([]byte(c.Cmd.Data), &metas)
 
-    for _, met := range metas {
-        toUid := helper.StrToInt(met.RoomKey[1:],-1)
-        if toUid < 1 {
-            continue
-        }
-        metRes := models.MessageSyncMeta{}
-        metRes.RoomKey = "u"+ helper.IntToStr(c.UserId)
-        metRes.ByUserId = c.UserId
-        metRes.MessageKey = met.MessageKey
-        metRes.AtTimeMs = met.AtTimeMs
+	for _, met := range metas {
+		toUid := helper.StrToInt(met.RoomKey[1:], -1)
+		if toUid < 1 {
+			continue
+		}
+		metRes := models.MessageSyncMeta{}
+		metRes.RoomKey = "u" + helper.IntToStr(c.UserId)
+		metRes.ByUserId = c.UserId
+		metRes.MessageKey = met.MessageKey
+		metRes.AtTimeMs = met.AtTimeMs
 
-        recivedCmd := base.NewCommand(constants.MsgsReceivedToPeer)
-        recivedCmd.AddSliceData(metRes)
-        recivedCmd.MakeDataReady()
+		recivedCmd := base.NewCommand(constants.MsgsReceivedToPeer)
+		recivedCmd.AddSliceData(metRes)
+		recivedCmd.MakeDataReady()
 
-        sync.AllPipesMap.SendAndStoreCmdToUser(toUid,recivedCmd)
+		sync.AllPipesMap.SendAndStoreCmdToUser(toUid, recivedCmd)
 
-        //send MsgDeletedFromServer to that user
-        delCmd := base.NewCommand(constants.MsgsDeletedFromServer)
-        delCmd.AddSliceData(metRes)
-        delCmd.MakeDataReady()
+		//send MsgDeletedFromServer to that user
+		delCmd := base.NewCommand(constants.MsgsDeletedFromServer)
+		delCmd.AddSliceData(metRes)
+		delCmd.MakeDataReady()
 
-        sync.AllPipesMap.SendAndStoreCmdToUser(toUid,delCmd)
-    }
+		sync.AllPipesMap.SendAndStoreCmdToUser(toUid, delCmd)
+	}
 
 }
 
 func MsgSeenByPeer(c *base.CmdAction) {
-    fmt.Println("called MsgSeenByPeer :",*c)
+	fmt.Println("called MsgSeenByPeer :", *c)
 
-    metas := []models.MessageSyncMeta{}
+	metas := []models.MessageSyncMeta{}
 
-    json.Unmarshal([]byte(c.Cmd.Data),&metas)
+	json.Unmarshal([]byte(c.Cmd.Data), &metas)
 
-    for _, met := range metas {
-        toUid := helper.StrToInt(met.RoomKey[1:],-1)
-        if toUid < 1 {
-            continue
-        }
-        metRes := models.MessageSyncMeta{}
-        metRes.RoomKey = "u"+ helper.IntToStr(c.UserId)
-        metRes.ByUserId = c.UserId
-        metRes.MessageKey = met.MessageKey
-        metRes.AtTimeMs = met.AtTimeMs
-        metRes.ExtraData = met.ExtraData
+	for _, met := range metas {
+		toUid := helper.StrToInt(met.RoomKey[1:], -1)
+		if toUid < 1 {
+			continue
+		}
+		metRes := models.MessageSyncMeta{}
+		metRes.RoomKey = "u" + helper.IntToStr(c.UserId)
+		metRes.ByUserId = c.UserId
+		metRes.MessageKey = met.MessageKey
+		metRes.AtTimeMs = met.AtTimeMs
+		metRes.ExtraData = met.ExtraData
 
-        recivedCmd := base.NewCommand(constants.MsgsSeenByPeer)
-        recivedCmd.AddSliceData(metRes)
-        recivedCmd.MakeDataReady()
+		recivedCmd := base.NewCommand(constants.MsgsSeenByPeer)
+		recivedCmd.AddSliceData(metRes)
+		recivedCmd.MakeDataReady()
 
-        sync.AllPipesMap.SendAndStoreCmdToUser(toUid,recivedCmd)
-    }
+		sync.AllPipesMap.SendAndStoreCmdToUser(toUid, recivedCmd)
+	}
 
 }
 
@@ -108,7 +107,6 @@ func MsgDeletedFromServer(c *base.CmdAction) {
 func MsgReceivedToServer(c *base.CmdAction) {
 
 }
-
 
 //func MsgAddNew(c *base.CmdAction) {
 //    fmt.Println("called MsgAddNew :",*c)
--- pipes/cmds_users_cmd.go
+++ pipes/cmds_users_cmd.go
@@ -1,26 +1,26 @@
 package pipes
 
 import (
-    "ms/sun/base"
-    "ms/sun/models"
-    "ms/sun/helper"
-"ms/sun/sync"
+	"ms/sun/base"
+	"ms/sun/helper"
+	"ms/sun/models"
+	"ms/sun/sync"
 )
 
-func GetUserForTable(c *base.CmdAction)  {
-    uid := helper.StrToInt(c.Cmd.Data,0)
-    if uid == 0{
-        return
-    }
-    print("data: ",uid)
+func GetUserForTable(c *base.CmdAction) {
+	uid := helper.StrToInt(c.Cmd.Data, 0)
+	if uid == 0 {
+		return
+	}
+	print("data: ", uid)
 
-    u:= models.GetUserById2(uid)
-    res := models.UserBasicAndMe{}
-    res.FormUserAndMe(u,c.UserId)
-    cmd := base.Command{
-        Name: "SetUserForTable",
-        Data: helper.ToJson(res),
-    }
+	u := models.GetUserById2(uid)
+	res := models.UserBasicAndMe{}
+	res.FormUserAndMe(u, c.UserId)
+	cmd := base.Command{
+		Name: "SetUserForTable",
+		Data: helper.ToJson(res),
+	}
 
-    sync.AllPipesMap.SendCmdToUser(c.UserId,&cmd)
+	sync.AllPipesMap.SendCmdToUser(c.UserId, &cmd)
 }
