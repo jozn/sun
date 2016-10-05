5353d7f36450a748473d8ac92aff10019877c744
Hamid <7atashfeshan@gmail.com>
2016-10-05 03:39:18 +0330

    added Call struct (copy of Command)

---
+++ base/call.go
@@ -0,0 +1,39 @@
+package base
+
+import (
+	"ms/sun/helper"
+	"time"
+)
+
+type Call struct {
+	Name            string
+	ClientNanoId    int64
+	ServerNanoId    int64
+	Data            string        //marshilized json - don't set dirctly set via toJsonData
+	toJsonSliceData []interface{} //`json:"-"`
+}
+
+func NewCall(Name string) *Call {
+	return &Call{Name: Name, ServerNanoId: time.Now().UnixNano()}
+}
+
+func (self *Call) SetData(data interface{}) {
+	self.Data = helper.ToJson(data)
+}
+
+func (self *Call) MakeDataReady() {
+	if self.toJsonSliceData != nil && len(self.toJsonSliceData) > 0 {
+		self.Data = helper.ToJson(self.toJsonSliceData)
+	}
+	//self.Data = helper.ToJson(data)
+}
+
+var CallMapRouter = make(map[string]func(*CmdAction))
+
+/*
+func (self *Call) AddSliceData(data interface{}) {
+	self.toJsonSliceData = append(self.toJsonSliceData, data)
+	//self.Data = helper.ToJson(data)
+}
+*/
+
