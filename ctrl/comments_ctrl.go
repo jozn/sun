1589b34523f04ce2d296db74d6fc6fefef29bdcc
Hamid <7atashfeshan@gmail.com>
2016-11-12 03:08:55 +0330

    comment_ctrl cleanups

--- ctrl/comments_ctrl.go
+++ ctrl/comments_ctrl.go
@@ -11,38 +11,6 @@ import (
 
 const COMMENTS_LIST_PAGE_LIMIT = 30
 
-//todo: suport pagination
-func GetCommentsAction_old(c *base.Action) base.AppErr {
-	//MustBeUser(c)
-	//devPrintn("action comments")
-	//time.Sleep(time.Second*1)
-	pidStr := c.Req.Form.Get("post_id")
-	pid := helper.StrToInt(pidStr, 0)
-
-	page := c.GetPage()
-	offset := page * COMMENTS_LIST_PAGE_LIMIT
-
-	var comments []models.Comment
-	err := base.DB.Select(&comments, "select * from comments where PostId = ? order by Id DESC limit ? OFFSET ? ", pid, COMMENTS_LIST_PAGE_LIMIT, offset)
-	// DB.Select(&comments, "select * from comments  limit 50 ") //, pid[0])
-
-	if err != nil {
-		helper.DebugErr(err)
-		return nil
-	}
-
-	var commentsInline []models.CommentInlineInfo
-	for _, cmt := range comments {
-		cmtView := models.CommentInlineInfo{}
-		cmtView.Comment = cmt
-		cmtView.Sender = models.GetUserView(cmt.UserId)
-		commentsInline = append(commentsInline, cmtView)
-	}
-	c.SendJson(commentsInline)
-	// c.SendJson(comments)
-	return nil
-}
-
 func GetCommentsAction(c *base.Action) base.AppErr {
 	UpdateSessionActivityIfUser(c)
 
@@ -88,16 +56,6 @@ func PostAddCommentAction(c *base.Action) base.AppErr {
 		return nil
 	}
 
-	/*cmt := models.Comment{}
-		cmt.UserId = c.UserId()
-
-		cmt.PostId = pid
-		cmt.CreatedTime = helper.TimeNow()
-		//todo format text for security and size ,parsing ,tages,...
-		cmt.Text = texts
-
-		base.DbInsertStruct(&cmt, "comments")
-	    models.QueryIncerPostCommentsCount(pid,1)*/
 	// c.SendText("ok")
 	cmt := models.CreateNewComment(c.UserId(), pid, texts)
 	c.SendJson(cmt)
@@ -115,4 +73,3 @@ func RemoveCommentAction(c *base.Action) base.AppErr {
 	return nil
 }
 
-/////////////////////////////////////////////////
