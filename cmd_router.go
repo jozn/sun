2b05b778dcaa471476a8700be46dbb53d36f7b90
Hamid <7atashfeshan@gmail.com>
2016-11-12 04:19:56 +0330

    cleanups of ws old routers

--- cmd_router.go
+++ cmd_router.go
@@ -8,30 +8,6 @@ import (
 
 //command events that clinet invokes
 func registerCmdRouters() {
-	/*
-		mp := base.CmdMapRouter
-
-		//commands reciced to client
-		mp["CommandsReceived"] = pipesold.CommandsReceived_Dep
-		mp["CommandsReceivedToClient"] = pipesold.CommandsReceived
-
-		//Messages flow
-		mp["MsgsAddNew"] = pipesold.MsgAddNew
-		mp["MsgsReceivedToServer"] = pipesold.MsgReceivedToServer
-		mp["MsgsReceivedToPeer"] = pipesold.MsgReceivedToPeer
-		mp["MsgsSeenByPeer"] = pipesold.MsgSeenByPeer
-		mp["MsgsDeletedFromServer"] = pipesold.MsgDeletedFromServer
-
-		mp["GetUserForTable"] = pipesold.GetUserForTable
-
-		//play
-		mp["hello"] = pipesold.HelloCmd
-		mp["echo"] = pipesold.EchoCmd
-
-		mp["AddNewMsg"] = pipesold.AddNewMsg
-
-		mp["EchoRes"] = pipesold.EchoRes
-	*/
 
 	mp2 := base.CallMapRouter
 	mp2["echo"] = models.EchoCmd
@@ -43,24 +19,3 @@ func registerCmdRouters() {
 
 }
 
-/*
-//commands reciced to client
-mp["CommandsReceived"] = cmd.CommandsReceived
-
-//Messages flow
-mp["MsgsAddNew"] = cmd.MsgAddNew
-mp["MsgsReceivedToServer"] = cmd.MsgReceivedToServer
-mp["MsgsReceivedToPeer"] = cmd.MsgReceivedToPeer
-mp["MsgsSeenByPeer"] = cmd.MsgSeenByPeer
-mp["MsgsDeletedFromServer"] = cmd.MsgDeletedFromServer
-
-mp["GetUserForTable"] = cmd.GetUserForTable
-
-//play
-mp["hello"] = cmd.HelloCmd
-mp["echo"] = cmd.EchoCmd
-
-mp["AddNewMsg"] = cmd.AddNewMsg
-
-mp["EchoRes"] = cmd.EchoRes
-*/
