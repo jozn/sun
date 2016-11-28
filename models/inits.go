820f26d613ac7aed8665ac9e765355538cadbb8d
Hamid <7atashfeshan@gmail.com>
2016-11-28 03:52:16 +0330

    remove old file + merge file (2 files)

--- models/inits.go
+++ models/inits.go
@@ -8,3 +8,26 @@ func init() {
 	//ReloadAllTags()
 
 }
+
+func OnAppStart_Models() {
+
+    UserMemoryStore.ReloadAll()
+    MemoryStore_User.ReloadAll()
+
+    ReloadTopUserIds()
+
+    ReloadAllTags()
+    ReloadTopTags()
+
+    go PeriodicReloadTopPostsForTopTags()
+
+    Cache = NewCache()
+    CacheModels = _cacheModels{Cache}
+
+    //todo: del this
+    p := Post{}
+    p.Text = "sadasdasd"
+
+    Cache.Set("post_12", &p, 0)
+}
+
