207a5dd8637d78ca8d09297fef704f58412c7281
Hamid <7atashfeshan@gmail.com>
2016-11-28 04:01:10 +0330

    router cleanups

--- router.go
+++ router.go
@@ -1,23 +1,19 @@
 package main
 
 import (
-	//		"ms/sun/fact"
 	. "ms/sun/actions"
 	"ms/sun/fact"
 	"net/http"
-	//"github.com/drone/routes"
 	"ms/sun/ctrl"
 	"ms/sun/models"
 	//"ms/sun/routes"
 	"fmt"
 	"github.com/dimfeld/httptreemux"
 	"ms/sun/base"
-	//"github.com/gorilla/mux"
+    "ms/sun/config"
 )
 
 func registerRoutes() *httptreemux.TreeMux {
-	//v1 := routes.NewPrefix("/v1")
-
 	v1Tree := httptreemux.New()
 	v1Tree.PanicHandler = func(w http.ResponseWriter, r *http.Request, p interface{}) {
 		fmt.Println("PANIC: ", p)
@@ -25,74 +21,63 @@ func registerRoutes() *httptreemux.TreeMux {
 	}
 	v1 := v1Tree.NewGroup("/v1")
 
-	http.Handle("/upload-avatar", actioner(UploadAvatarAction))
-	http.Handle("/remove-avatar", actioner(RemoveAvatarAction))
 
-	//http.Handle("/profile", actioner(GetProfileAction))
+    if config.IS_DEBUG {
 
-	//http.Handle("/search", actioner(SearchAllAction))
-	//http.Handle("/tag", actioner(SearchTagsAction))
-	//v1.POST("/sync-all-contacts", toV1(SyncAllContactsAction))
-	//v1.POST("/sync-followings", toV1(ctrl.SyncFollowingsAction))
-	//v1.GET("/sync-followings", toV1(ctrl.SyncFollowingsAction))
+        http.Handle("/upload-avatar", actioner(UploadAvatarAction))
+        http.Handle("/remove-avatar", actioner(RemoveAvatarAction))
 
-	http.Handle("/fact/user1", actioner(fact.FactUser1))
+        http.Handle("/fact/user1", actioner(fact.FactUser1))
 
-	http.Handle("/fact/user_real", actioner(fact.FactRealUser))
+        http.Handle("/fact/user_real", actioner(fact.FactRealUser))
 
-	v1.GET("/json2/:UserId", toV1(TestJson1))
-	v1.DELETE("/json2/:UserId", toV1(TestJson1))
+        v1.GET("/json2/:UserId", toV1(TestJson1))
+        v1.DELETE("/json2/:UserId", toV1(TestJson1))
 
-	http.Handle("/upload1", actioner(TestUpload1))
-	http.Handle("/upload3", actioner(PlayUpload3))
-	http.HandleFunc("/upload2", PlayUpload2)
+        http.Handle("/upload1", actioner(TestUpload1))
+        http.Handle("/upload3", actioner(PlayUpload3))
+        http.HandleFunc("/upload2", PlayUpload2)
 
-	//http.HandleFunc("/ws1", sync.ServeUserWs) //Deprectaed
-	//http.HandleFunc("/ws_call", pipes.ServeHttpWs)
-	http.HandleFunc("/ws_call", models.ServeHttpWs)
 
-	http.Handle("/mysql1", actioner(fact.IsamPlay))
-	http.Handle("/f/msg", actioner(fact.ChatMsgFact1))
-	http.Handle("/f/gm", actioner(fact.GroupMemFact1))
-	http.Handle("/f/ginfo", actioner(fact.GroupInfoFact1))
+        http.Handle("/mysql1", actioner(fact.IsamPlay))
+        http.Handle("/f/msg", actioner(fact.ChatMsgFact1))
+        http.Handle("/f/gm", actioner(fact.GroupMemFact1))
+        http.Handle("/f/ginfo", actioner(fact.GroupInfoFact1))
 
-	//phone dbs
-	http.Handle("/mf/contacts", actioner(fact.FactPhoneContacts))
+        //phone dbs
+        http.Handle("/mf/contacts", actioner(fact.FactPhoneContacts))
 
-	http.Handle("/ping", actionToFunc(ctrl.PingAction))
+        http.Handle("/ping", actionToFunc(ctrl.PingAction))
 
-	//http.Handle("/i/msg", actionToFunc(SendSampleMesgTable))
-	//http.Handle("/i/msg2", actionToFunc(SendSampleMesgTable2))
-	http.Handle("/i/msg", actionToFunc(ctrl.SendSampleMesgTable3_v04))
-	http.Handle("/i/redis", actionToFunc(RedisSavePlay))
-	http.Handle("/i/play", actionToFunc(PlaySomething))
-	http.Handle("/i/store1", actionToFunc(MemoryStore1))
-	http.Handle("/i/cache", actionToFunc(ShowCached))
-	http.Handle("/i/cacher", actionToFunc(ShowCacher))
-	http.Handle("/i/db", actionToFunc(DBStruct))
-	http.Handle("/i/java2", actionToFunc(DBStructsTojava))
-	http.Handle("/i/table", actionToFunc(DBStructsToTable))
-	//fixes
-	http.Handle("/i/fix_counts", actionToFunc(ctrl.FixAllCountsCounts))
-	http.Handle("/i/mem_user", actionToFunc(ctrl.DebugMemUser_ctrl))
-	http.Handle("/i/mem_user2", actionToFunc(ctrl.DebugMemUser_ctrl2))
-	http.HandleFunc("/i/java", DBStructsTojava2)
+        http.Handle("/i/msg", actionToFunc(ctrl.SendSampleMesgTable3_v04))
+        http.Handle("/i/redis", actionToFunc(RedisSavePlay))
+        http.Handle("/i/play", actionToFunc(PlaySomething))
+        http.Handle("/i/store1", actionToFunc(MemoryStore1))
+        http.Handle("/i/cache", actionToFunc(ShowCached))
+        http.Handle("/i/cacher", actionToFunc(ShowCacher))
+        http.Handle("/i/db", actionToFunc(DBStruct))
+        http.Handle("/i/java2", actionToFunc(DBStructsTojava))
+        http.Handle("/i/table", actionToFunc(DBStructsToTable))
+        //fixes
+        http.Handle("/i/fix_counts", actionToFunc(ctrl.FixAllCountsCounts))
+        http.Handle("/i/mem_user", actionToFunc(ctrl.DebugMemUser_ctrl))
+        http.Handle("/i/mem_user2", actionToFunc(ctrl.DebugMemUser_ctrl2))
+        http.HandleFunc("/i/java", DBStructsTojava2)
 
-	////////////// New Facts from v0.4 /////////////////
-	http.Handle("/fact/follow", actioner(fact.FactFollow))
-	http.Handle("/fact/unfollow", actioner(fact.FactUnFollow))
-	http.Handle("/fact/avatar", actioner(fact.FactUserAvatars))
-	http.Handle("/fact/like", actioner(fact.FactLike))
-	http.Handle("/fact/comment", actioner(fact.FactComment))
-	http.Handle("/fact/post", actioner(fact.FactPost))
-	http.Handle("/fact/about", actioner(fact.FactUpdateAboutMe))
+        ////////////// New Facts from v0.4 /////////////////
+        http.Handle("/fact/follow", actioner(fact.FactFollow))
+        http.Handle("/fact/unfollow", actioner(fact.FactUnFollow))
+        http.Handle("/fact/avatar", actioner(fact.FactUserAvatars))
+        http.Handle("/fact/like", actioner(fact.FactLike))
+        http.Handle("/fact/comment", actioner(fact.FactComment))
+        http.Handle("/fact/post", actioner(fact.FactPost))
+        http.Handle("/fact/about", actioner(fact.FactUpdateAboutMe))
+
+    }
 
 	//messages
-	//http.HandleFunc("/MsgUpload", MsgUpload)
 
 	//////////////////// New V1 apis //////////////////////////
-	//mux.POST("/grab_contacts", actionToFunc(ctrl.GrabAllUserContactsCtrl))
-	//mux.GET("/grab_contacts", actionToFunc(ctrl.GrabAllUserContactsCtrl))
 	http.Handle("/v1/grab_contacts", actionToFunc(ctrl.GrabAllUserContactsCtrl))
 
 	v1.POST("/post/add", toV1(ctrl.AddPostAction))
@@ -103,9 +88,7 @@ func registerRoutes() *httptreemux.TreeMux {
 	v1.GET("/post/delete", toV1(ctrl.PostDeleteAction))
 	v1.GET("/post/update", toV1(ctrl.PostUpdateAction))
 
-	//v1.GET("/follow", toV1(ctrl.FollowAction))
 	v1.POST("/follow", toV1(ctrl.FollowAction))
-	//v1.GET("/unfollow", toV1(ctrl.UnfollowAction))
 	v1.POST("/unfollow", toV1(ctrl.UnfollowAction))
 	v1.GET("/followers", toV1(ctrl.GetFollowersListAction))
 	v1.GET("/following", toV1(ctrl.GetFollowingsListAction))
@@ -123,12 +106,10 @@ func registerRoutes() *httptreemux.TreeMux {
 	v1.POST("/session/info", toV1(ctrl.SessionGetUserInfo))
 	v1.GET("/session/info", toV1(ctrl.SessionGetUserInfo))
 
-	//v1.GET("/profile/all", toV1(ctrl.GetPostsAndInfoForProfileAction)) //dep
 	v1.GET("/profile/posts", toV1(ctrl.GetPostsForProfileAction))
 	v1.GET("/profile/info", toV1(ctrl.GetProfileInfoAction))
 
 	v1.GET("/notifications", toV1(ctrl.VNoftificationsCtrl))
-	//mux.GET("notifications", toV1(ctrl.NoftificationsCtrl))
 
 	v1.GET("/recommend/top_posts", toV1(ctrl.RecommendPostsCtrl))
 	v1.GET("/recommend/users", toV1(ctrl.RecommendUsersCtrl))
@@ -136,7 +117,7 @@ func registerRoutes() *httptreemux.TreeMux {
 	v1.GET("/recommend/top_tags_discover", toV1(ctrl.RecommendTagsWithPostsCtrl))
 
 	v1.GET("/tags/list", toV1(ctrl.TagsPostsListCtrl))
-	//v1.GET("/search", toV1(ctrl.SearchCtrl))
+
 	v1.GET("/search/tags", toV1(ctrl.SearchTagsCtrl))
 	v1.GET("/search/users", toV1(ctrl.SearchUsersCtrl))
 	v1.GET("/search/cliked", toV1(ctrl.SearchClickedCtrl))
@@ -148,13 +129,11 @@ func registerRoutes() *httptreemux.TreeMux {
 
 	v1.GET("/sync_users", toV1(ctrl.SyncUsersCtrl))
 
-	///// v0.4 Msgs
+    http.HandleFunc("/ws_call", models.ServeHttpWs)
+    ///// v0.4 Msgs
 	http.HandleFunc("/msgs/v1/add_one", ctrl.MsgUploadV1)
 
-	//v1.GET("/like",toV1(ctrl.PostAddLikeAction))
-
 	http.Handle("/", v1Tree)
-	//http.Handle("/", mux)
 
 	return v1Tree
 
