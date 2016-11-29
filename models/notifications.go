7117cb181a5b08fe0a91ea6625c56e4ed22b8b04
Hamid <7atashfeshan@gmail.com>
2016-11-29 04:15:39 +0330

    notification_removed table + user_meta table

--- models/notifications.go
+++ models/notifications.go
@@ -21,6 +21,14 @@ type Notification struct {
 	_exists, _deleted bool
 }
 
+type __NotificationRemoved struct {
+    NotificationId int           `json:"NotificationId"` // NotificationId -
+    ForUserId      sql.NullInt64 `json:"ForUserId"`      // ForUserId -
+
+                                                         // xo fields
+    _exists, _deleted bool
+}
+
 //dep
 func (n *Notification) InsertToDb() {
 	res, err := base.DbInsertStruct(n, "notification")
